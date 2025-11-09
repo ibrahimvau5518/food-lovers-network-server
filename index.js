47779b2fc8b0ab0063b208f94b34fcc7ada106dc
ibrahimvau5518 <jibonm676@gmail.com>
2025-11-09 13:04:52 +0600

    Remove node_modules from tracking

--- index.js
+++ index.js
@@ -2,16 +2,83 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  'mongodb+srv://foodDbUser:4EyC0a3vijpltGtg@cluster0.4rgvatj.mongodb.net/?appName=Cluster0';
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get('/', (req, res) => {
   res.send('Food Lovers Network Server is running');
 });
 
-app.listen(port, () => { 
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('food_db');
+    const foodCollection = db.collection('foods');
+
+    app.get('/foods', async (req, res) => {
+      const cursor = foodCollection.find();
+      const foods = await cursor.toArray();
+      res.send(foods);
+    });
+
+    app.get('/foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/foods', async (req, res) => {
+      const newFood = req.body;
+      const result = await foodCollection.insertOne(newFood);
+      res.send(result);
+    });
+
+    app.delete('/foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch('/foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedFood = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedFood.name,
+          price: updatedFood.price,
+        },
+      };
+      const result = await foodCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
   console.log(`Food Lovers Network Server is running on port: ${port}`);
-})
+});
