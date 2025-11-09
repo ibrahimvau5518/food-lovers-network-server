b2cfd9a13e514eabe8945d58ef32fcaf4a7850b3
ibrahimvau5518 <jibonm676@gmail.com>
2025-11-09 12:21:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Food Lovers Network Server is running');
+});
+
+app.listen(port, () => { 
+  console.log(`Food Lovers Network Server is running on port: ${port}`);
+})
