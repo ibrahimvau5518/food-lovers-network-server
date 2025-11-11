86dda170c740345b590163c911a872685726dcb6
ibrahimvau5518 <jibonm676@gmail.com>
2025-11-11 11:19:03 +0600

    connect the db server in another db collection & remake the code

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB setup
 const uri =
   'mongodb+srv://foodDbUser:4EyC0a3vijpltGtg@cluster0.4rgvatj.mongodb.net/?appName=Cluster0';
 
@@ -19,66 +21,28 @@ const client = new MongoClient(uri, {
   },
 });
 
+// root route
 app.get('/', (req, res) => {
-  res.send('Food Lovers Network Server is running');
+  res.send('Food Review Server is running');
 });
 
 async function run() {
   try {
     await client.connect();
+    const db = client.db('localfoodlovers');
+    const reviewCollection = db.collection('reviews');
 
-    const db = client.db('food_db');
-    const foodCollection = db.collection('foods');
+   
 
-    app.get('/foods', async (req, res) => {
-      const cursor = foodCollection.find().sort({ price: 1 });
-      const foods = await cursor.toArray();
-      res.send(foods);
-    });
-
-    app.get('/foods/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await foodCollection.findOne(query);
-      res.send(result);
-    });
-
-    app.post('/foods', async (req, res) => {
-      const newFood = req.body;
-      const result = await foodCollection.insertOne(newFood);
-      res.send(result);
-    });
-
-    app.delete('/foods/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await foodCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    app.patch('/foods/:id', async (req, res) => {
-      const id = req.params.id;
-      const updatedFood = req.body;
-      const query = { _id: new ObjectId(id) };
-      const update = {
-        $set: {
-          name: updatedFood.name,
-          price: updatedFood.price,
-        },
-      };
-      const result = await foodCollection.updateOne(query, update);
-      res.send(result);
-    });
-
+    
     await client.db('admin').command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    );
+    console.log('Connected to MongoDB successfully!');
   } finally {
   }
 }
+
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Food Lovers Network Server is running on port: ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
